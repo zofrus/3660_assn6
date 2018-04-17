3a5a0f1afda330934e0a9e787bfba9331e1be0ce
zofrus <jasonfuller7@gmail.com>
2018-04-16 22:13:27 -0600

    final fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,25 +1,19 @@
 #include "assn6.h"
 
 void FCFS(int *requests, int count) {
-	 int ready[BLOCK_REQS]; 
-	 int clock = 0;
-	 int ta = 0, wait = 0, resp = 0;
-	 int next = 0, current = 0, insert = 0;
+	 int difference = 0, 
+	 	 	   next = 0, 
+			current = 0, 
+				  i = 0;
 
-	 while (next < count || current != insert) {
-	 	if(current == insert){
-		}
-	 	
-		while(next < count && requests[next] <= clock) {
-			ready[insert] = next;
-			next++;
-			insert++;	
-		}
+	while ( i < count - 1 ) {
+		current = requests[i];
+		next = requests[i + 1];
+		(next > current) ? 
+		(difference += next - current) : 
+		(difference += current - next);
+		i++;
+	}
 
-		if(requests[currburst] > 0) {
-		}
-		current++;
-	 }
-	 printf ("First Come, First Served\n");
-	 printf ("Avg. Resp.:%.2f, Avg. T.A.:%.2f, Avg. Wait:%.2f\n", (float)resp/count, (float)ta/count, (float)wait/count);
+	 printf ("FCFS Total Seek: %d\n", difference);
 }
