4b6e4260665589556c99cc7eeebdd2201b1377db
zofrus <jasonfuller7@gmail.com>
2018-04-19 21:57:05 -0600

    final push of algorithm related files

--- fcfs.c
+++ fcfs.c
@@ -1,3 +1,15 @@
+// Author: Jason Fuller
+/******** Group project **********/
+// CS 3060 Spring 2018
+// Assignment #6
+/* Promise of Originality
+I promise that this source code file has, in it's entirety, been
+written by myself and by no other person or persons. If at any time an
+exact copy of this source code is found to be used by another person in
+this term, I understand that both myself and the student that submitted
+the copy will receive a zero on this assignment.
+*/
+
 #include "assn6.h"
 
 void FCFS(int *requests, int count) {
